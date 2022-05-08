f0ae84a1c6a12873ac00eb4ef9bad7f335d51580
Liav <oneandonlyliav@gmail.com>
2022-05-08 07:19:35 -0400

    added basic component structure and routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Nav from './components/Nav';
+
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='app'>
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
