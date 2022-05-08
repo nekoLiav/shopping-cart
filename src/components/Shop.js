ea280a45950796af5fe589e63157ad80384daa62
Liav <oneandonlyliav@gmail.com>
2022-05-08 08:58:25 -0400

    basic styling for home and shop pages

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Shop = () => {
+  const [shopItem, setShopItem] = useState(0);
+
   return (
-    <div>
-      <h1>Shop</h1>
+    <div className='shop'>
+      <div className='shop-content'>
+        <h1>Shop</h1>
+        <button onClick={() => setShopItem(shopItem + 1)}>Click me!</button>
+      </div>
+      <div className='shopping-cart'>
+        <h3 className='cart-summary'>
+          {`Quantity:
+        ${shopItem}`}
+        </h3>
+      </div>
     </div>
   );
 };
