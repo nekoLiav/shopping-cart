cad3f296962d43660b195e275bfa9624a2dec281
Liav <oneandonlyliav@gmail.com>
2022-05-15 15:20:04 -0400

    move BrowserRouter to index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Checkout from './components/Checkout';
@@ -7,14 +7,12 @@ import Nav from './components/Nav';
 function App() {
   return (
     <div className='app'>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/shop' element={<Shop />} />
-          <Route path='/checkout' element={<Checkout />} />
-        </Routes>
-      </BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/checkout' element={<Checkout />} />
+      </Routes>
     </div>
   );
 }
